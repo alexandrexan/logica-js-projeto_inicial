ba658f9c164dc4654196e016cce1a6f97154ce82
alexandrexan <xampsbeatz@gmail.com>
2025-08-08 21:37:13 -0300

    estudos em js
    programação js

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 alert('Bem-vindo(a) ao jogo do número secreto!');
-let numeroSecreto = 92;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log('Número secreto:', numeroSecreto);
 let palpite = prompt('Adivinhe o número secreto entre 1 e 100:');
-let msgErro = 'Por favor, insira um número válido.';
+let msgErro = '⚠️ Por favor, insira um número válido.';
+let tentativas = 1;
 
-if (palpite == numeroSecreto) {
-    alert(`Parabéns! Você adivinhou o número secreto: ${numeroSecreto}`);
-} else {
-    alert(`Que pena! O número secreto era: ${numeroSecreto}`);
-
+while (palpite != numeroSecreto) {
+    if (palpite === null || palpite === "") {
+        alert(msgErro);
+    } else {
+        alert(`Que pena! Você errou. 😞`);
+        if (palpite < numeroSecreto) {
+            alert('Dica: O número secreto é maior que o seu palpite.');
+        } else {
+            alert('Dica: O número secreto é menor que o seu palpite.');
+        }
+        tentativas++;
+    }
+    palpite = prompt('Tente novamente. Adivinhe o número secreto entre 1 e 100:');
 }
+let msgTentativas = `Parabéns! 😃 Você adivinhou o número secreto: ${numeroSecreto} com ${tentativas} ${tentativas > 1 ? 'tentativas' : 'tentativa'}.`;
+alert(msgTentativas);
